37bf6144d9ebd3fb2ee8597d4096c99f03fa19a5
Enchan <enchant0130@gmail.com>
2021-09-12 12:56:23 +0900

    [Update] testcase

--- tests/test_ChainableIterator.py
+++ tests/test_ChainableIterator.py
@@ -43,5 +43,20 @@ class testChainableIterator(unittest.TestCase):
 
         self.assertEqual(list(datas_applied), ci_.as_list())
 
+    @termdecorate
+    def testApplyMultipleFunctions(self):
+        """ 複数の関数を同時に適用する """
+        ci_ = self.ci
+
+        fc_square = lambda n: n * 2
+        filter_even = lambda n: n % 2 == 0
+        ci_.filter(filter_even) \
+            .map(fc_square)
+
+        datas_applied = filter(filter_even, self.datas)
+        datas_applied = map(fc_square, datas_applied)
+
+        self.assertEqual(list(datas_applied), ci_.as_list())
+            
 if __name__ == '__main__':
     unittest.main()
