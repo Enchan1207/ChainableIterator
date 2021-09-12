7f78f08f402722e0e1f345ec68a3c7cc2d0aa307
Enchan <enchant0130@gmail.com>
2021-09-12 12:09:50 +0900

    [Add] testcases

---
+++ tests/test_ChainableIterator.py
@@ -0,0 +1,47 @@
+#
+# ChainableIteratorのテスト
+#
+
+import unittest
+import random
+
+from src.chainableiterator.chainableiterator import ChainableIterator
+from termdecorator import termdecorate
+
+
+class testChainableIterator(unittest.TestCase):
+
+    def setUp(self) -> None:
+        # 元データを生成しておく
+        self.datas = range(random.randint(1, 1000))
+        self.ci = ChainableIterator(self.datas)
+
+    @termdecorate
+    def testConvertToList(self):
+        """ list変換 """
+        self.assertEqual(list(self.datas), self.ci.as_list())
+    
+    @termdecorate
+    def testApplyMap(self):
+        """ map関数の適用 """
+        
+        # map
+        fc_square = lambda n: n * 2
+        ci_ = self.ci.map(fc_square)
+        datas_applied = map(fc_square, self.datas)
+
+        self.assertEqual(list(datas_applied), ci_.as_list())
+
+    @termdecorate
+    def testApplyFilter(self):
+        """ filter関数の適用 """
+
+        # filter
+        filter_even = lambda n: n % 2 == 0
+        ci_ = self.ci.filter(filter_even)
+        datas_applied = filter(filter_even, self.datas)
+
+        self.assertEqual(list(datas_applied), ci_.as_list())
+
+if __name__ == '__main__':
+    unittest.main()
